a737b9b42a7dbfd345f91c058d15db78eb03f1a7
sverrebeijl <153381@csgaugustinus.nl>
2023-04-17 09:44:20 +0000

    add data value

--- databases/test.sql
+++ databases/test.sql
@@ -6,4 +6,8 @@ CREATE TABLE `student` (
 `id` INT AUTO_INCREMENT PRIMARY KEY,
 `naam` TEXT NOT NULL,
 `klas` varchar(16) NOT NULL,
-)
+);
+
+INSERT INTO `student` (`id`,`naam`, `klas`) VALUES
+(1,'ALAN', 'Havo4a'),
+(2,'Bob', 'havo4b');
