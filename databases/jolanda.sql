688af53ae2e0874e3668781ba8269b12aaa9102a
sverrebeijl <153381@csgaugustinus.nl>
2023-04-19 10:06:25 +0000

    fix

--- databases/jolanda.sql
+++ databases/jolanda.sql
@@ -7,11 +7,10 @@ USE `jolanda`;
 CREATE TABLE `haarproducten` (
 `id` INT AUTO_INCREMENT PRIMARY KEY,
 `product` TEXT NOT NULL,
-`prijs` INT NOT NULL,
+`prijs` INT NOT NULL
 );
 
 INSERT INTO `haarproducten` (`id`,`product`, `prijs`) VALUES
-(1,'Shampoo', '15'),
-(2,'Conditioner', '20');
-(3,'Zilvershampoo','12');
-
+(1, 'Shampoo', '15'),
+(2, 'Conditioner', '20'),
+(3,'Zilvershampoo', '12');
