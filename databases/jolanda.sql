23e0e52b798e9ec086a7868c7e9d587e12765bee
sverrebeijl <153381@csgaugustinus.nl>
2023-04-19 09:49:11 +0000

    made db jolanda and table

---
+++ databases/jolanda.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXIST `jolanda`;
+
+CREATE DATABASE `jolanda`;
+
+USE `jolanda`;
+
+CREATE TABLE `haarproducten` (
+`id` INT AUTO_INCREMENT PRIMARY KEY,
+`product` TEXT NOT NULL,
+`prijs` INT NOT NULL,
+);
+
+INSERT INTO `haarproducten` (`id`,`product`, `prijs`) VALUES
+(1,'Shampoo', '15'),
+(2,'Conditioner', '20');
+(3,`Zilvershampoo`,`12`);
+
